11a41d584787d5c168b00bb3092967281b4f4255
McKenzie Fiege <mckenziefiege@McKenzies-MBP.home>
2018-10-07 09:09:45 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name) {
+  drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver() {
+  drivers.shift();
+}
+
+function appendDriver(name) {
+  let newarray
+  newarray = [...drivers, name];
+  return newarray;
+}
+
+function prependDriver(name) {
+  let newarray
+  newarray = [name, ...drivers];
+  return newarray;
+}
+
+
+function removeLastDriver() {
+  let newarray
+  newarray = drivers.slice(0, -1);
+  return newarray;
+}
+
+function removeFirstDriver() {
+  let newarray
+  newarray = drivers.slice(1);
+  return newarray;
+}
